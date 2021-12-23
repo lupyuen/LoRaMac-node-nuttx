3c90df2411081d69a2b6e0dcbb821616f4adae26
Lee Lup Yuen <luppy@appkaki.com>
2021-12-23 20:16:16 +0800

    Add Nuttx functions

--- src/nuttx.c
+++ src/nuttx.c
@@ -5,6 +5,41 @@
 #include <stdbool.h>
 
 ///////////////////////////////////////////////////////////////////////////////
+//  Board
+
+#include "boards/board.h"
+
+/*!
+ * \brief Initializes the mcu.
+ */
+void BoardInitMcu( void );
+
+/*!
+ * \brief Resets the mcu.
+ */
+void BoardResetMcu( void );
+
+/*!
+ * \brief Initializes the boards peripherals.
+ */
+void BoardInitPeriph( void );
+
+/*!
+ * \brief Gets the board 64 bits unique ID
+ *
+ * \param [IN] id Pointer to an array that will contain the Unique ID
+ */
+void BoardGetUniqueId( uint8_t *id );
+
+/*!
+ * \brief Manages the entry into ARM cortex deep-sleep mode
+ */
+void BoardLowPowerHandler( void );
+
+//  BoardCriticalSectionBegin
+//  BoardCriticalSectionEnd
+
+///////////////////////////////////////////////////////////////////////////////
 //  EEPROM
 
 #include "boards/eeprom-board.h"
@@ -38,16 +73,38 @@ LmnStatus_t EepromMcuReadBuffer( uint16_t addr, uint8_t *buffer, uint16_t size )
 ///////////////////////////////////////////////////////////////////////////////
 //  Real Time Clock
 
-//  RtcGetCalendarTime
-//  RtcBkupWrite
-//  RtcBkupRead
+#include "boards/rtc-board.h"
 
-///////////////////////////////////////////////////////////////////////////////
-//  Board
+/*!
+ * \brief Gets the system time with the number of seconds elapsed since epoch
+ *
+ * \param [OUT] milliseconds Number of milliseconds elapsed since epoch
+ * \retval seconds Number of seconds elapsed since epoch
+ */
+uint32_t RtcGetCalendarTime( uint16_t *milliseconds ) {
+    puts("TODO: RtcGetCalendarTime");
+    assert(false);
+    return 0;
+}
 
-//  BoardInitMcu
-//  BoardInitPeriph
-//  BoardLowPowerHandler
-//  BoardResetMcu
-//  BoardCriticalSectionBegin
-//  BoardCriticalSectionEnd
+/*!
+ * \brief Writes data0 and data1 to the RTC backup registers
+ *
+ * \param [IN] data0 1st Data to be written
+ * \param [IN] data1 2nd Data to be written
+ */
+void RtcBkupWrite( uint32_t data0, uint32_t data1 ) {
+    puts("TODO: RtcBkupWrite");
+    assert(false);
+}
+
+/*!
+ * \brief Reads data0 and data1 from the RTC backup registers
+ *
+ * \param [OUT] data0 1st Data to be read
+ * \param [OUT] data1 2nd Data to be read
+ */
+void RtcBkupRead( uint32_t* data0, uint32_t* data1 ) {
+    puts("TODO: RtcBkupRead");
+    assert(false);
+}
